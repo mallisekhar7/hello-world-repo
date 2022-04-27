4d7cf790d5f4491a6bc5e4f9a3601bd336e83fb7
Malli Sekhar <malsekha@publicisgroupe.net>
2022-04-27 16:39:12 +0530

    first commit

---
+++ src/test/java/com/sapient/endur/ui/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package com.sapient.endur.ui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+	
+}
